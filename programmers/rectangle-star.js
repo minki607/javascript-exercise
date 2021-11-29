8aaf4aa1866a50104ca934f4210350279a1dcab2
minki607 <suh@student.unimelb.edu.au>
2021-11-29 15:57:57 +0900

    feat: programmers algorithm +1

--- programmers/rectangle-star.js
+++ programmers/rectangle-star.js
@@ -1,1 +1,9 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12969
+
+function solution(n, m) {
+  const row = "*".repeat(n) + "\n";
+  const answer = row.repeat(m);
+  return answer;
+}
+
+// 프로그래머스에서 통과되려면 자바스크립트 표준 입출력 사용
